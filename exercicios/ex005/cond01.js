cde128cdd693e3e9f6a4a10b4e512a7fdd203327
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-07-10 15:37:19 -0300

    .

---
+++ exercicios/ex005/cond01.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é: ${vel}km/h`)
+
+if(vel >= 60){
+    console.log('Multa, ultrapassou a velocidade permitida')
+}
+    console.log('Dirija com segurança')
---
+++ exercicios/ex005/cond02.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+
+if(pais == 'Brasil'){
+    console.log('Faz o L')
+} else {
+    console.log('Faz o W')
+}
