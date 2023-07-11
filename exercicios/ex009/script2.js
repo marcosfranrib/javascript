793d0a1e0629abca73f371a8d53393098b6eda89
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-07-11 20:33:02 -0300

    .

--- exercicios/ex009/script2.js
+++ exercicios/ex009/script2.js
@@ -1,15 +1,56 @@
 function verificarPessoa(){
-   
-    var nasc = document.getElementById('ano')
-    var ano = nasc.value
-    var img = document.getElementById('imagem')
+    
+    var container_ano = new Date()
+    var ano_atual = container_ano.getFullYear()
+    var input_ano = document.getElementById('input_ano_nascimento')
+    var exibir = document.getElementById('p')
+    var imagem = document.getElementById('imagem')
+    var genero = document.getElementsByName('sexo')
+    var sexo = ''
+    var idade = ano_atual - Number(input_ano.value)
 
-    switch(ano){
-        case 2015:
-            img.src = 'imagens/cm.png'
-            img.style.display = 'block'
-        break;
+    if(input_ano.value.length == 0 || Number(input_ano.value) > ano_atual){
+       
+        window.alert('Erro, digite de novo')
+        exibir.innerHTML = 'Preencha os dados acima para ver o resultado.'
+        imagem.style.display = 'none'
+        
+
+    } else if(genero[0].checked && idade <= 10){
+            imagem.src = 'imagens/cm.png'
+            imagem.style.display = 'block'
+            exibir.innerHTML = `é um menino de ${idade} anos`
+        }
+
+        else if( genero[0].checked && idade > 18 && idade < 60){
+            imagem.src = 'imagens/ah.png'
+            imagem.style.display = 'block'
+            exibir.innerHTML = `é um homem de ${idade} anos`
+        }
+
+        else if( genero[0].checked && idade > 60){
+            imagem.src = 'imagens/ih.png'
+            imagem.style.display = 'block'
+            exibir.innerHTML = `é um homem idoso de ${idade} anos`
+        }
+
+        else if(genero[1].checked && idade <= 10){
+            imagem.src = 'imagens/cg.png'
+            imagem.style.display = 'block'
+            exibir.innerHTML = `é um menina de ${idade} anos`
+        }
+
+        else if( genero[1].checked && idade > 18 && idade < 60){
+            imagem.src = 'imagens/am.png'
+            imagem.style.display = 'block'
+            exibir.innerHTML = `é uma mulher de ${idade} anos`
+        }
+
+        else if( genero[1].checked && idade > 60){
+            imagem.src = 'imagens/im.png'
+            imagem.style.display = 'block'
+            exibir.innerHTML = `é um mulher idosa de ${idade} anos`
+        }
+
     }
 
-
-}
