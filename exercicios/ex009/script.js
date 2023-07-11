711278008e2fa3111b5306dcb5185504797b57e6
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-07-11 15:21:08 -0300

    .

---
+++ exercicios/ex009/script.js
@@ -0,0 +1,30 @@
+function carregar(){
+
+    //container de divs
+    var msg = document.getElementById('mensagem')
+    var img = document.getElementById('imagem')
+
+    //container de horas
+    var date = new Date()
+    var hora = 10
+
+    if (hora >= 6 && hora < 12){
+        msg.innerHTML = `Agora são ${hora} horas`
+        img.src = 'imagens/manha.jpg'
+
+    } else if (hora <= 18){
+        msg.innerHTML = `Agora são ${hora} horas`  
+        img.src = 'imagens/tarde.jpg' 
+        document.body.style.background = 'orange'
+
+    } else {
+        msg.innerHTML = `Agora são ${hora} horas`
+        img.src = 'imagens/noite.jpg'
+        document.body.style.background = '#4e06f7'
+    }
+
+}
+
+
+
+
