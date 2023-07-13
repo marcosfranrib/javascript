7e5ad3c69aa747895ac7fdda3d1612af91c027a8
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-07-13 13:53:50 -0300

    Create script.js

---
+++ exercicios/ex014/script.js
@@ -0,0 +1,13 @@
+
+var num = [4, 3, 2, 1]
+num.sort()
+
+//atalho exclusivo para arrays
+    for(let c in num){
+        console.log(num[c])
+    }
+
+// achar a posição de um numero especifico
+
+    let pos = num.indexOf(3)
+    console.log(`O valor 3 está na posição: ${pos}`)
